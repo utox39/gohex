787480c5aa4ede8222e206ac43f8ecce7f537631
utox39 <francescomoccaldi39@gmail.com>
2024-11-30 16:47:48 +0100

    Minor changes in the error message

--- utils/file_reader.go
+++ utils/file_reader.go
@@ -8,7 +8,7 @@ import (
 func ReadFile(path string) ([]byte, error) {
 	file, err := os.ReadFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("error opening the file: %s: %v\n", os.Args[1], err)
+		return nil, fmt.Errorf("error opening the file: %v\n", err)
 	}
 	return file, nil
 }
